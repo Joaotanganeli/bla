b2a6997bdcddb3e20f13082858c631fdcd8a119b
Joaotanganeli <49658808+Joaotanganeli@users.noreply.github.com>
2020-03-13 10:30:51 -0300

    Update Main.java

--- main/Main.java
+++ main/Main.java
@@ -1,9 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package main;
+package Main;
 
 /**
  *
@@ -16,12 +11,18 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        Conta c1 = new Conta(1000, "batata", 1);
-        Conta c2 = new Conta(3000, "esquerda", 2);
-        Conta c3 = new Conta(4000, "offnile", 3);
+          String[] contas = {"Conta c1", "Conta c2", "Conta c3"};
+          
+        for (int i = 0; i < contas.length; i++) {
+        Conta c1 = new Conta(contas[0],1000, "batata", 1);
+        Conta c2 = new Conta(contas[1],3000, "esquerda", 2);
+                    c2.creditar(100);
+        Conta c3 = new Conta(contas[2],4000, "offnile", 3);
         c1.almondega();
         c2.almondega();
-        c3.almondega();          
+        c3.almondega();
+        }
     }
+        
     
 }
